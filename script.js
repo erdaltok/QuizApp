9decfc8fdd6b932a4888d1f1cd46c75f99b9b30a
Erdal Tok <erdaltok@hotmail.de>
2023-09-26 09:31:06 +0200

    Layout and json array

---
+++ script.js
@@ -0,0 +1,34 @@
+let questions = [
+  {
+    "question": "Wer hat HTML erfunden?",
+    "answer_1": "Robbie Williams",
+    "answer_2": "Lady Gaga",
+    "answer_3": "Tim Berners-Lee",
+    "answer_4": "Justin Bieber",
+    "right_answer": 3
+  },
+  {
+    "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
+    "answer_1": "readonly",
+    "answer_2": "max",
+    "answer_3": "from",
+    "answer_4": "spellcheck",
+    "right_answer": 1
+    },
+  {
+    "question": "Wie wählst du alle Elemente vom Typ $%(%(& mit dem attribut title aus?",
+    "answer_1": "a[title]{...}",
+    "answer_2": "a > title {...}",
+    "answer_3": "a.title {...}",
+    "answer_4": "a-title {...}",
+    "right_answer": 1
+    },
+  {
+    "question": "Wie definiert man in Javascript eine Variable?",
+    "answer_1": "let 100 - rate;",
+    "answer_2": "100 - let rate;",
+    "answer_3": "rate - 100;",
+    "answer_4": "let rate = 100;",
+    "right_answer": 4
+    }  
+];
